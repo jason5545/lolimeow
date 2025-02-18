7e26a9ea74b8314f70be5f4637188b334e79cf66
Jui-Chen Chien <jason5545@gmail.com>
2025-02-18 16:29:05 +0800

    try to fix Noto

--- header.php
+++ header.php
@@ -64,7 +64,6 @@ if (!defined('ABSPATH')) {
             }
         </style>
     <?php } ?>
-<body style="font-family: 'Noto Sans TC', sans-serif;">
 <?php if (get_boxmoe('boxmoe_preloader')) { ?>
     <div class="preloader">
         <svg version="1.1" id="boxmoe-sakura" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"

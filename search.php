c769174a3b4d009cfb7ac74002b4f52c68b8ab65
Jui-Chen Chien <jason5545@gmail.com>
2025-03-04 14:18:11 +0800

    tc

--- search.php
+++ search.php
@@ -3,15 +3,15 @@
  * @package lolimeow@boxmoe themes
  * @link https://www.boxmoe.com
  */
-//=======安全设置，阻止直接访问主题文件=======
-if (!defined('ABSPATH')) {echo'Look your sister';exit;}
+//=======安全設定，阻止直接訪問主題文件=======
+if (!defined('ABSPATH')) { echo '看什麼看'; exit; }
 //=========================================
 get_header();
 ?>
       <section class="section-blog-breadcrumb container fadein-bottom">
         <div class="breadcrumb-head">
           <span>
-            <i class="fa fa-home"></i>Search</span>
+            <i class="fa fa-home"></i>搜尋</span>
         </div>
       </section>
 	  <section>
@@ -19,7 +19,7 @@ get_header();
 					<div class="site-search">
 						<h1 class="search-title">
 						<i class="fa fa-search"></i>
-						<span>搜索:[<?php echo htmlspecialchars($s); ?>]关键词<?php global $wp_query;echo ' 共' . $wp_query->found_posts . ' 篇文章';?></span></h1>
+						<span>搜尋:[<?php echo htmlspecialchars($s); ?>] 關鍵字<?php global $wp_query; echo ' 共' . $wp_query->found_posts . ' 篇文章';?></span></h1>
                 </div>
 		</div>
 		</section>
